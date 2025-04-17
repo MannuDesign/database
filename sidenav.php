f0c50106576c56d62e5b728fe85361398e656211
MannuKumar <mannukumar74000@gmail.com>
2025-04-17 22:36:00 +0530

    Add files via upload

---
+++ sidenav.php
@@ -0,0 +1,157 @@
+<div id="scrollbar">
+    <div class="container-fluid">
+
+
+        <div id="two-column-menu">
+        </div>
+        <ul class="navbar-nav" id="navbar-nav">
+            <li class="menu-title"><span data-key="t-menu">Menu</span></li>
+            <!-- <li class="nav-item">
+                <a class="nav-link menu-link" href="dashboard.php">
+                    <i class="ri-honour-line"></i> <span data-key="t-widgets">Dashboard</span>
+                </a>
+            </li> -->
+
+
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarPages1" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarPages1">
+                    <i class="ri-account-circle-line"></i> <span data-key="t-authentication">Manage CMS </span>
+                </a>
+                <div class="collapse menu-dropdown" id="sidebarPages1">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="about.php" class="nav-link" data-key="t-starter">About </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="contact.php" class="nav-link" data-key="t-privacy-policy"> Contact </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarPages2" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarPages2">
+                    <i class="ri-account-circle-line"></i> <span data-key="t-authentication">Manage Service</span>
+                </a>
+                <div class="collapse menu-dropdown" id="sidebarPages2">
+                    <ul class="nav nav-sm flex-column">
+
+
+                        <li class="nav-item">
+                            <a href="Add-Service.php" class="nav-link" data-key="t-term-conditions">Add Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="view-service.php" class="nav-link" data-key="t-term-conditions">View Service</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarLanding" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarLanding">
+                    <i class="ri-rocket-line"></i> <span data-key="t-landing">Manage Testimonials </span>
+                </a>
+                <div class="collapse menu-dropdown" id="sidebarLanding">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="Add-testimonial.php" class="nav-link" data-key="t-one-page">Add testimonials</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="view-testimonial.php" class="nav-link" data-key="t-nft-landing">View testimonials
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarBlogs" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarBlogs">
+                    <i class="ri-pencil-ruler-2-line"></i> <span data-key="t-base-ui">Manage Portfolio</span>
+                </a>
+                <div class="collapse menu-dropdown mega-dropdown-menu" id="sidebarBlogs">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="Add_Portfolio.php" class="nav-link" data-key="t-alerts">Add Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="view_Portfolio.php" class="nav-link" data-key="t-badges">View Portfolio</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarPartners" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarPartners">
+                    <i class="ri-pencil-ruler-2-line"></i> <span data-key="t-base-ui"> Partner </span>
+                </a>
+                <div class="collapse menu-dropdown mega-dropdown-menu" id="sidebarPartners">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="partners.php" class="nav-link" data-key="t-alerts">Add Partner</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="view-partners.php" class="nav-link" data-key="t-badges">View Partner</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <!-- First Menu -->
+            <!-- <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarTestimonials" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarTestimonials">
+                    <i class="ri-message-3-line"></i>
+                    <span data-key="t-base-ui">Manages Price</span>
+                </a>
+                <div class="collapse menu-dropdown mega-dropdown-menu" id="sidebarTestimonials">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="add-subscription.php" class="nav-link" data-key="t-alerts">Add Subscription</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="add-price.php" class="nav-link" data-key="t-alerts">Add Price</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="view-price.php" class="nav-link" data-key="t-badges">View Price</a>
+                        </li>
+                    </ul>
+                </div>
+            </li> -->
+
+            <!-- Second Menu -->
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarPriceManagement" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarPriceManagement">
+                    <i class="ri-money-dollar-circle-line"></i>
+                    <span data-key="t-base-ui">Manage FAQ </span>
+                </a>
+                <div class="collapse menu-dropdown mega-dropdown-menu" id="sidebarPriceManagement">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="faq.php" class="nav-link" data-key="t-alerts">Add FAQ </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="view-faq.php" class="nav-link" data-key="t-alerts">View FAQ</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+
+
+
+
+
+
+
+        </ul>
+    </div>
+    <!-- Sidebar -->
+</div>
